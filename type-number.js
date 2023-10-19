9b015cf89a30dba8639858c8be70a3d6e7360b18
Edinei da Motta Martinello <edineimm@weg.net>
2023-10-19 10:16:27 -0300

    tipos de dados

---
+++ type-number.js
@@ -0,0 +1,18 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero;
+
+console.log(soma);
+
+// tipo Number
+
+const multiplicacao = primeiroNumero * segundoNumero;
+
+console.log(multiplicacao);
+
+
---
+++ type-string.js
@@ -0,0 +1,32 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha2 = "minhaSenha123"
+console.log(senha2.length) // 13 caracteres
